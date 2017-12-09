9a0ea191cb78c3d8943897e30c77c1273881c3c0
wo_fpc <wo_fpc@outlook.com>
2017-12-09 15:57:59 +0800

    ui and loading level 1

---
+++ Assets/Scripts/Mananger/SceneController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    public SceneField mainScene;
+    public SceneField level1Scene;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void BackToMenu()
+    {
+        LoadScene(mainScene);
+    }
+
+    public void StartGame()
+    {
+        LoadScene(level1Scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(SceneField scene)
+    {
+        StartCoroutine(LoadingScene(scene));
+    }
+
+    IEnumerator LoadingScene(SceneField scene)
+    {
+        Debug.Log("loading " + scene);
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(scene);
+        while (!asyncOp.isDone)
+        {
+            yield return null;
+        }
+    }
+}
